b4dc8835d396e0a1ba86d1764f5bea41217d57ec
Gobinda002 <152700064+Gobinda002@users.noreply.github.com>
2024-07-18 21:53:01 +0545

    asd

--- Assets/PauseScript.cs
+++ Assets/PauseScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseScript : MonoBehaviour
 {
@@ -17,6 +18,20 @@ public class PauseScript : MonoBehaviour
     public void Continue()
     {
         pause.SetActive(false);
+        Time.timeScale = 1;
+    }
+    
+    public void Restart()
+    {
+        pause.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1;
+    }
+
+    public void Home()
+    {
+        pause.SetActive(false);
+        SceneManager.LoadSceneAsync(0);
         Time.timeScale = 1;
     }
 }
