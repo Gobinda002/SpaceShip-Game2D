38ab3c7d4d5835cbfde88def0bfd8bb0a4b5880d
Gobinda002 <152700064+Gobinda002@users.noreply.github.com>
2024-07-18 19:51:16 +0545

    sdf

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -2,13 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject exitPanel;
+    public Button yesButton;
+    public Button noButton;
     public void StartGame()
     {
-        SceneManager.LoadSceneAsync(0); // Replace with your game scene name
+        SceneManager.LoadSceneAsync(1); 
     }
+    /*public void ShowExitPanel()
+    {
+        exitPanel.SetActive(false);
+    }*/
+
+    public void ShowExitPannel()
+    {
+        exitPanel.SetActive(false);
+    }
+
 
     public void QuitGame()
     {
